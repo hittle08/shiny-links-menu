553c631ae05a8f098e673dfac19141c0538326dc
hittle08 <hittle08@gmail.com>
2020-02-19 11:00:33 -0500

    Inital push

---
+++ js.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    //creating a variable for wrappers height
+    var wideToHeight = $(".wrapper").height() 
+
+    //setting h3's width to wrappers height
+    $("H3").css({ 'width': wideToHeight})
+
+
+});
