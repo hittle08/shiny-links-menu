0e6a6256d380d128b8ba7db264d350a6b3000dda
hittle08 <hittle08@gmail.com>
2020-02-19 12:50:46 -0500

    double columns 7x7

--- js.js
+++ js.js
@@ -2,10 +2,10 @@
 $(document).ready(function () {
 
     //creating a variable for wrappers height
-    var wideToHeight = $(".wrapper").height() 
+    var wideToHeight = $(".wrapper").height(); 
 
     //setting h3's width to wrappers height
-    $("H3").css({ 'width': wideToHeight})
+    $("H3").css({ 'width': wideToHeight});
 
 
 });
